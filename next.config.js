258632152f2a4e72e3608dbaff8852e71e67a2cd
Beatriz de Moraes Silva <beatriz.silva@biti9.com.br>
2024-09-18 18:57:59 -0300

    corrigindo build

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {};
 
-module.exports = nextConfig
+module.exports = {
+  output: "export", // Isso indica que o Next.js será exportado como estático
+  basePath: "/aranhaversobea.github.io", // Substitua pelo nome do seu repositório GitHub
+  trailingSlash: true, // O GitHub Pages requer "trailing slash"
+};
